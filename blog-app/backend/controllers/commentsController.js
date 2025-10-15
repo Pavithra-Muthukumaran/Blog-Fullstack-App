41f095bd81e13684122cf610c47385ff7eef3e87
Pavithra-Muthukumaran <poongothaipavi2001@gmail.com>
2025-10-15 23:40:12 +0530

    backend

---
+++ blog-app/backend/controllers/commentsController.js
@@ -0,0 +1,24 @@
+import Comment from '../models/Comments.js';
+
+export const addComments = async (req, res) => {
+    try{
+        const {content} = req.body;
+        const comment = new Comment({content, author : req.user.id, post : req.params.postId});
+        await comment.save();
+        res.status(201).json(await comment.populate('author', 'name')); 
+    }catch(err){
+        res.status(500).json({message : 'Server Error'});
+    }
+};
+
+export const deleteComment = async (req, res) =>{
+    try{
+        const comment = await Comment.findById(req.params.id);
+        if(!comment) return res.status(404).json({message : 'Not Found'});
+        if(comment.author.toString() !== req.user.id) return res.status(403).json({message : 'Forbidden'});
+        await comment.remove();
+        res.json({message : 'Deleted'});
+    }catch(err){
+        res.status(500).json({message : 'Server Error'});
+    }
+};
